8c60679390001a6e68c5e265c3f3ebe87724e8db
tim <sfzombie13@users.noreply.github.com>
2019-09-26 09:41:04 -0400

    Delete CredClear.bat

--- CredClear.bat
+++
@@ -1,5 +0,0 @@
-cmdkey.exe /list > "%TEMP%\List.txt"
-findstr.exe Target "%TEMP%\List.txt" > "%TEMP%\tokensonly.txt"
-FOR /F "tokens=1,2 delims= " %%G IN (%TEMP%\tokensonly.txt) DO cmdkey.exe /delete:%%H
-del "%TEMP%\List.txt" /s /f /q
-del "%TEMP%\tokensonly.txt" /s /f /q
